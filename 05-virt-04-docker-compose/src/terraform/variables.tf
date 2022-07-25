2282edcef2b994937b0647df5dce6768f4e4bd25
Andrey Maltsev <malzew@gmail.com>
2022-07-25 23:33:06 +0500

    Homework 5.3 [05-virt-04]

---
+++ 05-virt-04-docker-compose/src/terraform/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1g2n2okvr289487oale"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1gg33hbpnn7at4oorel"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd8h98gekij0r35u3g2p"
+}
